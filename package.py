0cc160dcd3d1e5bb1bd4d412860629ff7585e6de
mulaskij <mulaskij@gmail.com>
2023-03-05 14:38:26 -0500

    initial commit

---
+++ package.py
@@ -0,0 +1,11 @@
+
+class Package:
+    def __init__(self, ID, address, deadline, city, zipcode, weight, status):
+        self.ID = ID
+        self.dAddress = address
+        self.pDeadline = deadline
+        self.city = city
+        self.zipcode = zipcode
+        self.weight = weight
+        self.status = status
+
